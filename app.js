5c476822f2b6bd09020aae776422fdab23b14c4e
Luigi <luigi.prodesign@gmail.com>
2019-04-25 03:57:11 +0200

    Added major changes to index and style

---
+++ app.js
@@ -0,0 +1,34 @@
+const glyphs = ['->','::','=>','www','!!','!=','__','<=','>=','===','.=','||','<-','..','|>','&&','<<','>>','<>','#{','--',':=','...','-->','.-','~@','#_(',';;','#(','??','..<','=='];
+
+
+/* for (let i = 0; i < 9; i++) {
+    const languages = document.getElementsByClassName('language');
+} */
+
+/* const test = document.getElementsByClassName('language')[0].textContent;
+console.log(test.replace(/\s/g, '')); */
+
+/* glyphs.forEach(function(x) {
+    for (let i = 0; i < 9; i++) {
+        const languages = document.getElementsByClassName('language')[i].textContent;
+        languages.replace(/\s/g, '');
+        for (let a = 0; a < languages.length; a++) {
+            if (x ==)
+        }
+    }    
+}); */
+
+
+
+let search = ( str, what ) => {
+	return '"' + what + '" was found ' + str.match( new RegExp( what, 'g' )).length + ' times';
+}
+
+/* let search = ( str, arr ) => {
+    arr.forEach(function(glyph) {
+        console.log('"' + glyph + '" was found ' + str.match( new RegExp( glyph, 'g' )) + ' times');
+    })
+} */
+
+search("classCar<ActiveRecord::Basehas_many:wheels,:class_name=>'Wheel',:foreign_key=>'car_id'scope:available,->{where(available:true)}endomega=->{'www'}alpha=->(arg){arg*2}hash={1=>'one',2=>'two'}","->");
+
---
+++ app[Conflicto].js
@@ -0,0 +1,13 @@
+const glyphs = ['->','::','=>','www','!!','!=','__','/*','*/','<=','>=','++','===','.=','||','<-','..','|>','&&','<<','>>','<>','#{','--',':=','...','-->','.-','~@','#_(',';;','#(','??','..<','=='];
+
+var email = document.getElementsByClassName('email')[0];
+
+console.log(codeExamples);
+/* for (let i = 0; i < 9; i++) {
+    const codeExamples = document.getElementsByClassName('language');
+    console.log(codeExamples[i]);
+} */
+
+
+
+
